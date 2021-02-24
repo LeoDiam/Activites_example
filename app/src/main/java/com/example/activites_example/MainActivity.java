e62db3099ce6ce037a4442a4bf6e8b1f6721830f
KotrotsKon <kotrotskon@users.noreply.github.com>
2021-02-24 20:19:26 +0200

    add methods for file cycle

--- app/src/main/java/com/example/activites_example/MainActivity.java
+++ app/src/main/java/com/example/activites_example/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,14 +15,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("LIFE_CYCLE", "MainActivity - onCreate");
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("BUTTON", "You click a button");
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
-                MainActivity.this.finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LIFE_CYCLE", "MainActivity - onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LIFE_CYCLE", "MainActivity - onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LIFE_CYCLE", "MainActivity - onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LIFE_CYCLE", "MainActivity - onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LIFE_CYCLE", "MainActivity - onDestroy");
     }
 }
--- app/src/main/java/com/example/activites_example/SecondActivity.java
+++ app/src/main/java/com/example/activites_example/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.activites_example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,37 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Log.d("LIFE_CYCLE", "SecondActivity - onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LIFE_CYCLE", "SecondActivity - onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LIFE_CYCLE", "SecondActivity - onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LIFE_CYCLE", "SecondActivity - onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LIFE_CYCLE", "SecondActivity - onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LIFE_CYCLE", "SecondActivity - onDestroy");
     }
 }
